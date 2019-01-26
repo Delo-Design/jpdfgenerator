5ee0aa48afa754caab55d7c7c42caa1a8b121d96
tsymb <tsymbal.mitia@gmail.com>
2019-01-26 22:22:24 +0300

    Стартовые файлы

---
+++ jpdfgenerator.php
@@ -0,0 +1,106 @@
+<?php
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\FileLayout;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Database\DatabaseDriver;
+
+/**
+ * Jpdfgenerator plugin.
+ *
+ * @package  pdfrealty
+ * @since    1.0
+ */
+class plgSystemJpdfgenerator extends CMSPlugin
+{
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0
+	 */
+	protected $autoloadLanguage = true;
+
+
+	public function onAjaxJpdfgenerator()
+    {
+        $app = Factory::getApplication();
+        $request_data = $app->input->getArray();
+        $html = '';
+        $action = 'stream';
+        $template_name = 'default';
+        $template_path_default = JPATH_ROOT . implode(DS, ['plugins', 'system', 'jpdfgenerator', 'tmpl']);
+        $template_path_theme = JPATH_ROOT . DS . 'templates' . DS . $app->getTemplate() . implode(DS, ['html', 'plg_system_jpdfgenerator']);
+
+        //проверяем есть ли кастомный шаблон в запросе
+        if(isset($request_data['template']))
+        {
+            $template_name = $request_data['template'];
+        }
+
+        //проверяем есть ли действие в запросе
+        if(isset($request_data['action']))
+        {
+            $action = $request_data['action'];
+        }
+
+        //проверяем если ли кастомный шаблон
+        if(file_exists($template_path_theme . DS . $template_name))
+        {
+            $template = new FileLayout('template', $template_path_theme . DS . $template_name);
+
+            if(!file_exists($template_path_theme . DS . $template_name . DS . 'data.php'))
+            {
+                echo Text::_('PLG_JPDFGENERATOR_TEMPLATE_DATA_NOT_FOUND');
+                $app->close();
+            }
+
+            $template_data = include $template_path_theme . DS . $template_name . DS . 'data.php';
+            $html = $template->render($template_data);
+        }
+        else
+        {
+
+            //провереяем есть ли стандартные шаблоны
+            if(file_exists($template_path_default . DS . $template_name))
+            {
+                $template = new FileLayout('template', $template_path_default . DS . $template_name);
+
+                if(!file_exists($template_path_default . DS . $template_name . DS . 'data.php'))
+                {
+                    echo Text::_('PLG_JPDFGENERATOR_TEMPLATE_DATA_NOT_FOUND');
+                    $app->close();
+                }
+
+                $template_data = include $template_path_default . DS . $template_name . DS . 'data.php';
+                $html = $template->render($template_data);
+            }
+            else
+            {
+                //выкидываем ошибку, если шаблон вообще не найден
+                echo Text::_('PLG_JPDFGENERATOR_TEMPLATE_NOT_FOUND');
+                $app->close();
+            }
+
+        }
+
+        JLoader::register('JMpdf', JPATH_LIBRARIES . '/mpdf/jmpdf.php');
+        $pdf = JMpdf::getPdf($html);
+
+        if($action === 'stream')
+        {
+
+        }
+        $pdf->stream();
+
+        $app->close();
+
+    }
+
+
+}
---
+++ tmpl/default/template.php
@@ -0,0 +1,41 @@
+<?php defined('_JEXEC') or die; ?>
+
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Карточки</title>
+    </head>
+
+    <style>
+        @page {
+            margin: 35px;
+        }
+
+        *,html,body
+        {
+            font-family: "Times New Roman";
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            text-align: center;
+            font-size: 11px;
+            padding: 5px 10px;
+        }
+
+        .desc {
+            text-align: left;
+            padding: 10px;
+        }
+
+
+    </style>
+
+    <body>
+        <p>Hello world</p>
+    </body>
+</html>
